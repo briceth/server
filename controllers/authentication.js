19936e64a0a9d5aee56e2581ad6f818a813f1605
Brice Tessier Huort <tessierhuort@gmail.com>
2017-02-06 19:26:01 +0100

    add some token

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,4 +1,12 @@
 const User = require('../models/user');
+const jwt = require('jwt-simple');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timeStamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timeStamp }, config.secret);
+}
+
 
 exports.signup = function(req, res, next) {
   const email = req.body.email;
@@ -25,7 +33,7 @@ exports.signup = function(req, res, next) {
     user.save(function(err) {
       if(err) { return next(err); }
       //repond to request indicating  the user was created
-      res.json({ success: true });
+      res.json({ token: tokenForUser(user) });
     });
   });
 }
--- models/user.js
+++ models/user.js
@@ -2,22 +2,31 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema; //??
 const bcrypt = require('bcrypt-nodejs');
 
+
 // Define our model
 const UserSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   password: { type: String, unique: true }
 
 });
+//On save hook, encrypt password
+//before saving a model, run this function
 UserSchema.pre('save', function(next) {
-  const user = this;
+  //get access to the user
+  const user = this; //user.email, user.password
+
+  // I - generate a salt then run callback
   bcrypt.genSalt(10, function(err, salt) {
     if(err) { return next(err); }
-    
+
+// II - hash(encrypt) our password using the salt
     bcrypt.hash(user.password, salt, null, function(err, hash) {
       if(err) { return next(err); }
 
+      //overwrite plain text password with encrypted password
       user.password = hash;
-      next();
+
+      next(); // go for it you can save
     });
   });
 });
